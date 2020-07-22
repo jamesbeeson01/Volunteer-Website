0489f62d80f50218524a0d893b52df650d585708
jamesbeeson01 <jamesbeeson01@gmail.com>
2020-07-21 18:53:21 -0600

    Adding form

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
 	function(){
 		$(this).css("color", "red");
 	});
+	
+	$("#new_project_button").hover(function(){
+		$(this).css("background-color", "#565656");
+	},
+	function(){
+		$(this).css("background-color", "#4ad0e3");
+	});
+	
+	$("#new_project_button").onclick(function(){
+		$("#new_project_form").css("display", "block");
+	});
+	
+	$("#close_button").onclick(function(){
+		$("#new_project_form").css("display", "none");
+	});
 });

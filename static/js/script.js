28d2b05a3794f8a3b1d7ce6a8c732d741e3a1d67
jamesbeeson01 <jamesbeeson01@gmail.com>
2020-07-30 17:13:06 -0600

    Improve UI
    Added text that appears when the create new project button is hovered over.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
 	$("p").hover(function(){
 		$(this).css("color", "yellow");
 	}, 
@@ -8,9 +9,11 @@ $(document).ready(function(){
 	
 	$("#new_project_button").hover(function(){
 		$(this).css("background-color", "#565656");
+		$("#new_project_text").css("display", "block");
 	},
 	function(){
 		$(this).css("background-color", "#4ad0e3");
+		$("#new_project_text").css("display", "none");
 	});
 	
 	$("#new_project_button").click(function (){
